2a422c663dca34b6862655695544a98318255466
Matthieu Carpentier <matthieu.carpentier75@gmail.com>
2018-01-15 15:20:10 +0100

    fix(category): access control api routes

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -4,15 +4,33 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource
+ *      attributes={
+ *          "normalization_context"={"groups"={"read"}},
+ *          "denormalization_context"={"groups"={"write"}}
+ *      },
+ *      collectionOperations={
+ *          "get"={"method"="GET", "access_control"="is_granted('ROLE_USER')"},
+ *          "post"={"method"="POST", "access_control"="is_granted('ROLE_ADMIN')"}
+ *      },
+ *      itemOperations={
+ *          "get"={"method"="GET", "access_control"="is_granted('ROLE_USER')"},
+ *          "put"={"method"="PUT", "access_control"="is_granted('ROLE_ADMIN')"},
+ *          "delete"={"method"="DELETE", "access_control"="is_granted('ROLE_ADMIN')"}
+ *      }
+ * )
  *
  * @ORM\Entity
  * @ORM\Table(name="category")
+ * @UniqueEntity(fields={"name"}, message="Name is already taken")
  */
 class Category
 {
@@ -22,6 +40,8 @@ class Category
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      * @ORM\Column(type="integer")
+     *
+     * @Groups({"read"})
      */
     private $id;
 
@@ -30,17 +50,21 @@ class Category
      *
      * @ORM\Column(type="string")
      *
+     * @Groups({"read", "write"})
+     *
      * @Assert\NotBlank()
      */
     private $name;
 
     /**
      * @ORM\ManyToMany(targetEntity="User", mappedBy="categories")
+     * @ApiSubresource()
      */
     private $users;
 
     /**
      * @ORM\ManyToMany(targetEntity="Restaurant", mappedBy="categories")
+     * @ApiSubresource()
      */
     private $restaurants;
 
