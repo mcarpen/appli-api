baf275718587621a3eeda4c712834d28fe5f1d20
Matthieu Carpentier <matthieu.carpentier75@gmail.com>
2018-01-15 14:52:46 +0100

    Behat

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,69 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\SchemaTool;
+
+/**
+ * This context class contains the definitions of the steps used by the demo
+ * feature file. Learn how to get started with Behat and BDD on Behat's website.
+ *
+ * @see http://behat.org/en/latest/quick_start.html
+ */
+class FeatureContext implements Context
+{
+    /**
+     * @var ManagerRegistry
+     */
+    private $doctrine;
+
+    /**
+     * @var EntityManager
+     */
+    private $manager;
+
+    /**
+     * @var SchemaTool
+     */
+    private $schemaTool;
+
+    /**
+     * @var array
+     */
+    private $classes;
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     * @param ManagerRegistry $doctrine
+     */
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+        $this->manager = $doctrine->getManager();
+        $this->schemaTool = new SchemaTool($this->manager);
+        $this->classes = $this->manager->getMetadataFactory()->getAllMetadata();
+    }
+
+    /**
+     * @BeforeScenario @createSchema
+     *
+     * @throws \Doctrine\ORM\Tools\ToolsException
+     */
+    public function createDatabase()
+    {
+        $this->schemaTool->createSchema($this->classes);
+    }
+
+    /**
+     * @AfterScenario @dropSchema
+     */
+    public function dropDatabase()
+    {
+        $this->schemaTool->dropSchema($this->classes);
+    }
+}
